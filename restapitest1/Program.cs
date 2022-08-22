1270a990270b0272cc70c8f78c80911ad85a71ee
Paddy <paddy.zemann@gmail.com>
2022-08-23 00:47:27 +0200

    Projektdateien hinzufügen.

---
+++ restapitest1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace restapitest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://swapi.dev/api/people/";
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+
+            string response = httpResponse.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(response);
+
+            People swapi = JsonConvert.DeserializeObject<People>(response);
+            
+            Console.WriteLine(swapi);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ restapitest1/Swapi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restapitest1
+{
+    public class People
+    {
+        public string name;
+        public string height;
+        public string hair_color;
+    }
+}
